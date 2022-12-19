628216304a98102524d15417eaa1e2dce4a694b2
robertvlad <vladrob96@gmail.com>
2022-12-19 01:27:34 +0100

    Milestone 0

---
+++ js/script.js
@@ -0,0 +1,43 @@
+//MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+
+const teams = [
+    {
+        name: "Wayner Barnett",
+        role: "Founder & CEO",
+        image: "wayner-barnertt-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Walter Gorgon",
+        role: "Office Manager",
+        image: "waalter-gorgon-office-manager.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg",
+    }    
+]
+
+
+//MILESTONE 1: Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+
+
+
+
+//MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
